6d7c7e5b4a2009ee2a765e5746377b102badcc4f
Luis Perez <luisperez@Lisaidas-Air.neo.rr.com>
2019-04-28 09:04:30 -0400

    new files

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,21 @@
-import React, {Component} from 'react';
-//import logo from './logo.svg';
+import React, { Component } from 'react';
+import Person from './Person/Person';
 import './App.css';
 
-class App extends Component() {
+class App extends Component {
+  
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This looks pretty cool</p>
+        <button>Switch Name</button>
+        <Person name='Luis' age='29'/>
+        <Person name='Sadie' age='29'/>
+        <Person name='Celkay' age='6'> I love you girls very much!</Person>
       </div>
     );
   }
-
 }
 
 export default App;
