4df6c761413985a85759be9c52e4e1d6f5343962
luisperez <luisperez@bravowell.com>
2019-04-30 14:57:03 -0400

    added timer and turn count to card game#

--- javascript/cardgame.js
+++ javascript/cardgame.js
@@ -6,9 +6,16 @@ $(document).ready(function() {
 	let memoryValue = [0,0,1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10,11,11,12,12,13,13,14,14,15,15];
 	let testOne = 0;
 	let turns = 0;
+	let tries = 0;
+	let seconds = 0;
+	let minutes = 0;
+	
+	
 	let doesItMatch = [];
 
 	function startLayout(column, row) {
+		$('#timer').text("Timer: 0:00");
+		$('#card_flips').text("Turns: 0");
 	  for (outer = 0; outer < column; outer++){
 		for (inner = 0; inner < row; inner++){
 		  testOne++;
@@ -22,11 +29,24 @@ $(document).ready(function() {
 		}
 	  }
 	}
+	function timerIncrement() {
+		let leading_zero = '0';
+		seconds++;
+		
+		if(seconds > 9){ leading_zero = '';}
+		if(seconds == 60){
+			minutes++;
+			seconds = 0;
+		}
+		$('#timer').text("Timer: " + minutes + ":" + leading_zero + seconds);
+	}
 	startLayout(cardLayout.column, cardLayout.row);
+	setInterval(timerIncrement, 1000);
 	$('.card-value').hide();
 	$('.col-2').click(function() {
 		let selectedCard = $(this).attr('id');
 		let checkValue = $(this).children('.card-value').data('test');
+		
 		doesItMatch.push(checkValue);
 		console.log(doesItMatch);
 		turns++;
@@ -50,7 +70,10 @@ $(document).ready(function() {
 			}
 			turns = 0;
 			doesItMatch= [];
+			tries++;
 		}
+		
+		$('#card_flips').text("Turns: " + tries);
 
 	})
 });
