0ee8d5baac7a9774591d5ec93b4bd23069e35b95
Luis Perez <luisperez1050@outlook.com>
2020-09-17 23:41:08 -0400

    cleaned up code and enhanced reset button also added 60 minute limit

--- javascript/cardgame.js
+++ javascript/cardgame.js
@@ -6,13 +6,14 @@ $(document).ready(function() {
 		column: 6,
 		row: 6
 	};
-	let memoryValue = [0,0,1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10,11,11,12,12,13,13,14,14,15,15];
+
 	let cardValue = 0;
 	let turns = 0;
 	let tries = pad(0);
 	let seconds = 0;
 	let minutes = 0;
 	let points = 0;
+	let timeIntervalID = '';
 	
 	
 	let doesItMatch = [];
@@ -22,7 +23,7 @@ $(document).ready(function() {
 	*/
 
 	document.getElementById('start').addEventListener('click', () => {
-		setInterval(timerIncrement, 1000);
+		timeIntervalID = setInterval(timerIncrement, 1000);
 		document.querySelector('.row').classList.remove('disabled');
 		document.getElementById('start').setAttribute('disabled', '');
 	});
@@ -30,66 +31,71 @@ $(document).ready(function() {
 	document.getElementById('reset').addEventListener('click', () => {
 		seconds, minutes, tries, points = 0;
 		timerIncrement(true);
-		document.querySelector('.row').innerHTML = '';
+		document.querySelector('.row').innerHTML = ''; //clear out the cards to start over
 		startLayout(cardLayout.column, cardLayout.row);
-		//need to iterate every element
-		document.querySelectorAll('.card-value').forEach(card => card.classList.add('hide'));
+
+		document.querySelector('.row').classList.add('disabled');
+		document.getElementById('start').removeAttribute('disabled');
+		clearInterval(timeIntervalID);
 	});
 	
 	startLayout(cardLayout.column, cardLayout.row);
 	
-	document.querySelectorAll('.card-value').forEach(card => card.classList.add('hide'));
+	// attach click events and determine if clicked cards match
+	function attachCardFlipEvents() {
+		document.querySelectorAll('.card-value').forEach(card => card.classList.add('hide'));
 
-	const cards = document.querySelectorAll('.col-4');
-	[...cards].forEach( card => {
-		card.addEventListener('click', () => {
-			let selectedCard = card.id;
-			let checkValue = card.querySelector('.card-value').dataset.card_value;
-			doesItMatch.push(checkValue);
-			console.log(doesItMatch);
-			turns++;
-
-			document.getElementById(selectedCard).querySelector('.card-container').classList.toggle('hide');
-			document.getElementById(selectedCard).querySelector('.card-container').classList.add('selected');
-			document.getElementById(selectedCard).querySelector('.card-value').classList.toggle('hide');
-
-			if (turns == 2) {
-				//document.querySelector('.col-4').style.cssText = "pointer-events: none";
-				[...cards].forEach( card => card.style.pointerEvents = 'none');
-				if (doesItMatch[0] === doesItMatch[1]){
-					points++;
-					points = pad(points);
-					setTimeout(function() {
-						document.getElementById('points').innerHTML = `Points: ${points}`;
-						document.querySelectorAll(`.selected ~ [data-card_value="${checkValue}"] span`).forEach( match => match.innerHTML = 'MATCH');
-						document.querySelectorAll(`.selected ~ [data-card_value="${checkValue}"]`).forEach( selected => selected.classList.add('card-match'));
-						document.querySelectorAll(`.selected ~ [data-card_value="${checkValue}"]`).forEach( selected => selected.classList.remove('card-value'));
-						[...cards].forEach( card => card.style.pointerEvents = 'all');
-						document.querySelectorAll('div.selected').forEach( selected => selected.remove());
+		const cards = document.querySelectorAll('.col-4');
+		[...cards].forEach( card => {
+			card.addEventListener('click', () => {
+				let selectedCard = card.id;
+				let checkValue = card.querySelector('.card-value').dataset.card_value;
+				doesItMatch.push(checkValue);
+				console.log(doesItMatch);
+				turns++;
+	
+				document.getElementById(selectedCard).querySelector('.card-container').classList.toggle('hide');
+				document.getElementById(selectedCard).querySelector('.card-container').classList.add('selected');
+				document.getElementById(selectedCard).querySelector('.card-value').classList.toggle('hide');
+	
+				if (turns == 2) {
+					[...cards].forEach( card => card.style.pointerEvents = 'none');
+					if (doesItMatch[0] === doesItMatch[1]){
+						points++;
+						points = pad(points);
+						setTimeout(function() {
+							document.getElementById('points').innerHTML = `Points: ${points}`;
+							document.querySelectorAll(`.selected ~ [data-card_value="${checkValue}"] span`).forEach( match => match.innerHTML = 'MATCH');
+							document.querySelectorAll(`.selected ~ [data-card_value="${checkValue}"]`).forEach( selected => selected.classList.add('card-match'));
+							document.querySelectorAll(`.selected ~ [data-card_value="${checkValue}"]`).forEach( selected => selected.classList.remove('card-value'));
+							[...cards].forEach( card => card.style.pointerEvents = 'all');
+							document.querySelectorAll('div.selected').forEach( selected => selected.remove());
+							
+							if(points == 18){
+								document.getElementById('points').innerHTML = " <h2> You win Yay! Reset Game to Start Over</h2>";
+							}
+						}, 1200);
 						
-						if(points == 18){
-							document.getElementById('points').innerHTML = " <h2> You win Yay! Reset Game to Start Over</h2>";
-						}
-					}, 1200);
-					
-				} else {
-					setTimeout(function() {
-						document.querySelectorAll('.card-container').forEach( container => container.classList.remove('hide'));
-						document.querySelectorAll('.card-value').forEach( val => val.classList.add('hide'));
-						document.querySelectorAll('.card-container').forEach( container => container.classList.remove('selected'));
-						[...cards].forEach( card => card.style.pointerEvents = 'all');
-					}, 1200);
+					} else {
+						setTimeout(function() {
+							document.querySelectorAll('.card-container').forEach( container => container.classList.remove('hide'));
+							document.querySelectorAll('.card-value').forEach( val => val.classList.add('hide'));
+							document.querySelectorAll('.card-container').forEach( container => container.classList.remove('selected'));
+							[...cards].forEach( card => card.style.pointerEvents = 'all');
+						}, 1200);
+					}
+					turns = 0;
+					doesItMatch= [];
+					tries++;
+					tries = pad(tries);
 				}
-				turns = 0;
-				doesItMatch= [];
-				tries++;
-				tries = pad(tries);
-			}
-			
-			document.getElementById('card_flips').innerHTML = `Turns: ${tries}`;
-
+				
+				document.getElementById('card_flips').innerHTML = `Turns: ${tries}`;
+	
+			});
 		});
-	});
+	}
+
 
 	/*
 	* F U N C T I O N S
@@ -115,6 +121,7 @@ $(document).ready(function() {
 			}
 		}
 		shuffle();
+		attachCardFlipEvents();
 	}
 	// borrowed from JS fiddle
     function shuffle(){
@@ -150,8 +157,16 @@ $(document).ready(function() {
 
 		if(seconds == 60){
 			minutes++;
+			seconds = 0;
+			
 			minutes = pad(minutes);
 			seconds = pad(0);
+		}
+
+		if(minutes == 60) {
+			document.querySelector('.row').classList.add('disabled');
+			document.querySelectorAll('.col-4').forEach(card => card.style.pointerEvents = 'none');8
+			clearInterval(timeIntervalID);
 		}
 
 		document.getElementById('timer').innerHTML = `Timer: ${minutes}:${seconds}`;
